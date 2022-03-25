7b3552ada121c904d18a9989cb57f3933a09d32c
manishMandal02 <manishmandalj@gmail.com>
2022-03-25 19:36:10 +0530

    minor fix

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res, next) => {
-  res.send('Server is running');
+  res.send('Server is running...');
 });
 
 app.use('/api/bitcoinPrice', priceRoutes);
