452ed55b03b4cd88c43f9638548edaf18e8a490d
Manish Mandal <manishmandalj@gmail.com>
2023-05-09 16:15:00 +0530

    fix: mongodb connect

--- src/config/db.js
+++ src/config/db.js
@@ -2,15 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.bold);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error connecting to db: ${error.message}`.red.underline.bold);
+    console.error(`Error connecting to db: ${error.message}`);
     process.exit(1);
   }
 };
